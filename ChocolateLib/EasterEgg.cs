1f3651248667847b4f58c49101d76aba71d64a82
Klaus Eigil Erik Avgo Thor Therkildsen <klau8393@edu.zealand.dk>
2024-05-29 19:31:45 +0200

    Add project files.

---
+++ ChocolateLib/EasterEgg.cs
@@ -0,0 +1,15 @@
+﻿namespace ChocolateLib
+{
+    public class EasterEgg
+    {
+        public int ProductNo { get; set; }
+        public string ChocolateType { get; set; }
+        public int Price { get; set; }
+        public int InStock { get; set; }
+
+        public override string ToString()
+        {
+            return $"ProductNo: {ProductNo}, ChocolateType: {ChocolateType}, Price: {Price}, InStock: {InStock}";
+        }
+    }
+}
